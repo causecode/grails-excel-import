f516717379da15cbe14b1070dbc23b0ece981a6f
priyanshuch <priyanshu.chauhan@causecode.com>
2016-05-20 11:37:32 +0530

    Plugin descriptor fixed.

--- src/main/groovy/grails/excelimport/GrailsExcelImportGrailsPlugin.groovy
+++ src/main/groovy/grails/excelimport/GrailsExcelImportGrailsPlugin.groovy
@@ -1,10 +1,12 @@
 package grails.excelimport
 
-class GrailsExcelImportGrailsPlugin {
+import grails.plugins.*
+
+class GrailsExcelImportGrailsPlugin extends Plugin {
     // the plugin version
     def version = "1.1.0.BUILD-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "3.1.4"
+    def grailsVersion = "3.1.4 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
@@ -33,30 +35,31 @@ class GrailsExcelImportGrailsPlugin {
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/excel-import"
 
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
+    Closure doWithSpring() { {->
+            // TODO Implement runtime spring config (optional)
+        }
     }
 
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
+    void doWithDynamicMethods() {
         // TODO Implement registering dynamic methods to classes (optional)
     }
 
-    def doWithApplicationContext = { applicationContext ->
+    void doWithApplicationContext() {
         // TODO Implement post initialization spring config (optional)
     }
 
-    def onChange = { event ->
+    void onChange(Map<String, Object> event) {
         // TODO Implement code that is executed when any artefact that this plugin is
         // watching is modified and reloaded. The event contains: event.source,
         // event.application, event.manager, event.ctx, and event.plugin.
     }
 
-    def onConfigChange = { event ->
+    void onConfigChange(Map<String, Object> event) {
         // TODO Implement code that is executed when the project configuration changes.
         // The event is the same as for 'onChange'.
+    }
+
+    void onShutdown(Map<String, Object> event) {
+        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
